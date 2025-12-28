c4d9f06eb847e13a48c78e655f71491ce0af112c
junxianghuang827-png <junxianghuang827@gmail.com>
2025-12-29 00:10:10 +0800

    Add files via upload

---
+++ frc2025/sw.js
@@ -0,0 +1,16 @@
+// sw.js - 讓 App 支援離線讀取
+const CACHE_NAME = 'frc-scout-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
